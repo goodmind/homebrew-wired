556d071db84e7550a305e3fa0ba86815d2651d9a
andretshurotshka <andwebar@gmail.com>
2017-07-22 21:00:04 +0500

    feat(infornography): update to v0.5

--- Formula/infornography.rb
+++ Formula/infornography.rb
@@ -1,8 +1,8 @@
 class Infornography < Formula
   desc "Racket script to show system information"
   homepage "https://github.com/goodmind/infornography"
-  url "https://github.com/goodmind/Infornography/releases/download/v0.4/infornography-darwin-0.4.r0.ga97ad3c.tar.gz"
-  version "0.4"
+  url "https://github.com/goodmind/Infornography/releases/download/v0.5/infornography-darwin-0.5.r0.g718c351.tar.gz"
+  version "0.5"
   sha256 "e94acf766d1ee97b817965ec8975a422856dd18cf8ef9128a5100df32be7f984"
 
   def install
