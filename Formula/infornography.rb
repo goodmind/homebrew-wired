980c5385a5851b02cd617f5bc82c86f6c6e3103b
andretshurotshka <andwebar@gmail.com>
2018-03-25 22:12:13 +0500

    Use Carp version

--- Formula/infornography.rb
+++ Formula/infornography.rb
@@ -1,13 +1,13 @@
 class Infornography < Formula
-  desc "Racket script to show system information"
+  desc "Carp script to show system information"
   homepage "https://github.com/goodmind/infornography"
-  url "https://github.com/goodmind/Infornography/releases/download/v0.5/infornography-darwin-0.5.r0.g718c351.tar.gz"
-  version "0.5"
+  url "https://github.com/goodmind/infornography-carp/releases/download/v0.7/infornography-osx-0.7.r0.ga137b8a.tar.gz"
+  version "0.7"
   sha256 "2ddbb0ce92c19d722b907445ba6721046ff1d1bcf50944c88b289526be610879"
 
   def install
     bin.install "bin/infornography"
-    lib.install Dir["lib/*"]
+    # lib.install Dir["lib/*"]
   end
 
   test do
